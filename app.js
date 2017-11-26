79f144fd998bc9b29f951bd3c20838c7165adf87
Aleksei <ale12062@gmail.com>
2017-11-26 03:50:39 +0200

    some jade stuff

--- app.js
+++ app.js
@@ -68,8 +68,8 @@ var auth = require('./routes/auth');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
